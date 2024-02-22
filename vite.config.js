e2e27db255ce2f4035c201fb9b8412a5ce9bdbd4
jxes6102 <jxes6102@gamil.com>
2024-02-22 18:11:09 +0800

    config - 1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/drawPicture/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
